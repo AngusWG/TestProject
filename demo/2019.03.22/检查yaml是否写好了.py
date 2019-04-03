25cfd85bab5bab47c3c5f74f8f7bf7c7abfba420
zza <740713651@qq.com>
2019-04-03 17:23:24 +0800

    daily update

--- demo/2019.03.22/检查yaml是否写好了.py
+++ demo/2019.03.22/检查yaml是否写好了.py
@@ -3,8 +3,19 @@
 # @Time    : 2019/2/19 18:26
 # @Author  : zza
 # @Email   : 740713651@qq.com
-import yaml
+from ruamel import yaml
 
-r = open("config.yaml", "r", encoding="utf8")
-a = r.read()
-print(yaml.load(a)["data"]["config.py"])
+config_yaml_path = r"C:\Users\admin\Desktop\fsdownload\config.yaml"
+new_yaml_path = r"C:\Users\admin\Desktop\fsdownload\new_config.yaml"
+py_config_path = r"C:\Users\admin\Desktop\fsdownload\config.py"
+
+file_dict = yaml.load(open(config_yaml_path, "r", encoding="utf8").read())
+py_file = open(py_config_path, 'r', encoding="utf-8").read()
+
+# 检查py代码
+compile(py_file, "config,py", "exec")
+file_dict["data"]["config.py"] = py_file
+with open(new_yaml_path, "w", encoding="utf-8") as f:
+    yaml.dump(file_dict, f,Dumper=yaml.RoundTripDumper)
+
+print("finish")
--- 工具/发送邮件.py
+++ 工具/发送邮件.py
@@ -5,11 +5,14 @@
 # @Email   : 740713651@qq.com
 import yagmail
 
+
 # 连接邮箱服务器
-yag = yagmail.SMTP(user="lijianzhaoyou@163.com", password="填自己的", host='smtp.163.com')
+def sen_email():
+    yag = yagmail.SMTP(user="1169254038@qq.com", password="vkyvmovizdsiigja", host='smtp.q.com')
+    # 邮箱正文
+    contents = ['今天是周末,我要学习, 学习使我快乐;', '<a href="https://www.python.org/">python官网的超链接</a>', './girl.jpg']
+    # 发送邮件
+    yag.send('740713651@qq.com', '主题:学习使我快乐', contents)
 
-# 邮箱正文
-contents = ['今天是周末,我要学习, 学习使我快乐;', '<a href="https://www.python.org/">python官网的超链接</a>', './girl.jpg']
-
-# 发送邮件
-yag.send('zhaoolee@gmail.com', '主题:学习使我快乐', contents)
+    # yagmail.SMTP(user="1169254038@qq.com", password="vkyvmovizdsiigja", host='smtp.qq.com'). \
+    #     send('740713651@qq.com', 'finish task xxx', ["finish task"])
