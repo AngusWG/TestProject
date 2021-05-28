fc36e5f85443e313910fba47538d61447949e0f6
AngusWG <740713651@qq.com>
2021-05-28 13:37:45 +0800

    daily update

--- demo/fastapi_websocket_2.py
+++ demo/fastapi_websocket_2.py
@@ -4,8 +4,9 @@
 
 # https://stackoverflow.com/questions/65361686/websockets-stream-audio-in-json-out-w-fastapi-and-two-websockets
 import asyncio
+from typing import Optional
 
-from fastapi import FastAPI, WebSocket
+from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -19,20 +20,30 @@ html = """
     <body>
         <h1>WebSocket Chat</h1>
         <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
+            <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
+            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+            <button onclick="connect(event)">Connect</button>
+            <hr>
+            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
             <button>Send</button>
         </form>
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
+        var ws = null;
+            function connect(event) {
+                var itemId = document.getElementById("itemId")
+                var token = document.getElementById("token")
+                ws = new WebSocket("ws://localhost:8000/items/" + itemId.value + "/ws?token=" + token.value);
+                ws.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
+                event.preventDefault()
+            }
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
                 ws.send(input.value)
@@ -43,6 +54,12 @@ html = """
     </body>
 </html>
 """
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
 
 data_list = []
 
@@ -61,17 +78,27 @@ async def recv(ws_a: WebSocket, ):
         print("websocket A sent:", data_list)
 
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+async def get_cookie_or_token(
+        websocket: WebSocket,
+        session: Optional[str] = Cookie(None),
+        token: Optional[str] = Query(None),
+):
+    if session is None and token is None:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/items/{item_id}/ws")
+async def websocket_endpoint(
+        websocket: WebSocket,
+        item_id: str,
+        q: Optional[int] = None,
+        # cookie_or_token: str = Depends(get_cookie_or_token),
+):
     await websocket.accept()
     fwd_task = asyncio.create_task(send(websocket))
     rev_task = asyncio.create_task(recv(websocket))
-
+    await asyncio.gather(fwd_task, rev_task)
 
 
 if __name__ == '__main__':
